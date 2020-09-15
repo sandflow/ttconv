5034cefe03f5bc7726a3aceecb9ec30aa892aada
Bob Brown <bob@bigleafdesigns.com>
2020-09-15 13:46:50 -0600

    Adding main.py with argument parsing (input file, output file) - (#7)

---
+++ src/main/python/ttconv/main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright (c) 2020, Sandflow Consulting LLC
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''ttconv main'''
+
+import re
+import logging
+import ttconv.model as model
+
+LOGGER = logging.getLogger(__name__)
+
+import sys, getopt
+
+#
+# Parse input arguments
+#
+
+def parse_args(argv):
+  inputfile = ''
+  outputfile = ''
+
+  try:
+    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+  except getopt.GetoptError:
+    print('main.py -i <inputfile> -o <outputfile>')
+    sys.exit(2)
+  
+  for opt, arg in opts:
+    if opt == '-h':
+        print('main.py -i <inputfile> -o <outputfile>')
+        sys.exit()
+    elif opt in ("-i", "--ifile"):
+        inputfile = arg
+    elif opt in ("-o", "--ofile"):
+        outputfile = arg
+  
+  print('Input file is ', inputfile)
+  print('Output file is ', outputfile)
+
+  return inputfile, outputfile
+
+#
+# Process input and output through the reader, converter, and writer
+#
+
+def process(inputfile, outputfile):
+
+  print('Input file is ', inputfile)
+  print('Output file is ', outputfile)
+
+
+def main(argv):
+  inputfile = ''
+  outputfile = ''
+  
+  inputfile, outputfile = parse_args(argv)
+  
+  process(inputfile, outputfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
