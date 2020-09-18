dccb46c041d2b0f8ec46dbab22b6ce9747bb9937
Valentin NOËL <valent.noel@gmail.com>
2020-09-18 18:02:18 +0200

    Add SCC reader (#6)
    Add SCC reader (#5)

--- src/main/python/ttconv/scc/mid_row_codes.py
+++ src/main/python/ttconv/scc/mid_row_codes.py
@@ -23,19 +23,47 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-'''Unit tests for the IMSC reader'''
+"""SCC Mid Row Codes"""
 
-# pylint: disable=R0201,C0115,C0116
+import typing
+from enum import Enum
 
-import unittest
-import xml.etree.ElementTree as et
-from ttconv.imsc import imsc_reader
 
-class IMSCReaderTest(unittest.TestCase):
+class SccMidRowCode(Enum):
+  """SCC Mid-Row Code values"""
+  WHITE = (0x1120, 0x1920)
+  WHITE_UNDERLINE = (0x1121, 0x1921)
+  GREEN = (0x1122, 0x1922)
+  GREEN_UNDERLINE = (0x1123, 0x1923)
+  BLUE = (0x1124, 0x1924)
+  BLUE_UNDERLINE = (0x1125, 0x1925)
+  CYAN = (0x1126, 0x1926)
+  CYAN_UNDERLINE = (0x1127, 0x1927)
+  RED = (0x1128, 0x1928)
+  RED_UNDERLINE = (0x1129, 0x1929)
+  YELLOW = (0x112A, 0x192A)
+  YELLOW_UNDERLINE = (0x112B, 0x192B)
+  MAGENTA = (0x112C, 0x192C)
+  MAGENTA_UNDERLINE = (0x112D, 0x192D)
+  ITALICS = (0x112E, 0x192E)
+  ITALICS_UNDERLINE = (0x112F, 0x192F)
 
-  def test_body_only(self):
-    tree = et.parse('src/test/resources/ttml/body_only.ttml')
-    imsc_reader.to_model(tree)
+  def __init__(self, channel_1_value: int, channel_2_value: int):
+    self._channel_1 = channel_1_value
+    self._channel_2 = channel_2_value
 
-if __name__ == '__main__':
-  unittest.main()
+  def get_name(self) -> str:
+    """Retrieves SCC Mid-Row Code name"""
+    return self.name
+
+  def contains_value(self, value: int) -> bool:
+    """Returns whether the specified value is contained into the Mid-Row Code channel values"""
+    return value in [self._channel_1, self._channel_2]
+
+
+def find_mid_row_code(value: int) -> typing.Optional[SccMidRowCode]:
+  """Find the Mid-Row Code corresponding to the specified value"""
+  for mid_row_code in list(SccMidRowCode):
+    if mid_row_code.contains_value(value):
+      return mid_row_code
+  return None
