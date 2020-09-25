249f753778af521e7d48181e0c2ad4b23ddfaa45
Valentin NOEL <valent.noel@gmail.com>
2020-09-25 11:25:04 +0200

    SCC: reader refactoring to set text to model

--- src/main/python/ttconv/scc/scc_reader.py
+++ src/main/python/ttconv/scc/scc_reader.py
@@ -24,12 +24,15 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """SCC reader"""
+from __future__ import annotations
 
 import re
 import logging
-from typing import List
-from ttconv.model import Document, P, Body, Div
 from ttconv.scc import time_codes
+from typing import Optional, List
+
+from ttconv.model import Document, P, Body, Div, Text, Span
+from ttconv.scc.time_codes import SccTimeCode
 
 LOGGER = logging.getLogger(__name__)
 
@@ -45,85 +48,118 @@ class _SccContext:
     self.count = 0
 
 
-def _is_hex_word(word: str) -> bool:
-  """Checks whether the specified word is a 2-bytes hexadecimal word"""
-  if len(word) != 4:
-    return False
-  try:
-    int(word, 16)
-  except ValueError:
-    return False
-  return True
+class SccWord:
+  """SCC hexadecimal word definition"""
+
+  def __init__(self):
+    self.value = None
+    self.byte_1 = None
+    self.byte_2 = None
+
+  @staticmethod
+  def _is_hex_word(word: str) -> bool:
+    """Checks whether the specified word is a 2-bytes hexadecimal word"""
+    if len(word) != 4:
+      return False
+    try:
+      int(word, 16)
+    except ValueError:
+      return False
+    return True
+
+  @staticmethod
+  def _decipher_parity_bit(byte: int):
+    """Extracts the byte value removing the odd parity bit"""
+    return byte & PARITY_BIT_MASK
+
+  @staticmethod
+  def from_value(value: int) -> SccWord:
+    """Creates a SCC word from the specified integer value"""
+    if value > 0xFFFF:
+      raise ValueError("Expected a 2-bytes int value, instead got ", hex(value))
+    data = value.to_bytes(2, byteorder='big')
+    return SccWord.from_bytes(data[0], data[1])
+
+  @staticmethod
+  def from_bytes(byte_1: int, byte_2: int) -> SccWord:
+    """Creates a SCC word from the specified bytes"""
+    if byte_1 > 0xFF or byte_2 > 0xFF:
+      raise ValueError(f"Expected two 1-byte int values, instead got {hex(byte_1)} and {hex(byte_2)}")
+    scc_word = SccWord()
+    scc_word.byte_1 = SccWord._decipher_parity_bit(byte_1)
+    scc_word.byte_2 = SccWord._decipher_parity_bit(byte_2)
+    scc_word.value = scc_word.byte_1 * 0x100 + scc_word.byte_2
+
+    return scc_word
+
+  @staticmethod
+  def from_str(hex_word: str) -> SccWord:
+    """Extracts hexadecimal word bytes to create a SCC word"""
+    if not SccWord._is_hex_word(hex_word):
+      raise ValueError("Expected a 2-bytes hexadecimal word, instead got ", hex_word)
+
+    data = bytes.fromhex(hex_word)
+    return SccWord.from_bytes(data[0], data[1])
+
+  def to_text(self) -> str:
+    """Converts SCC word to text"""
+    return ''.join([chr(self.byte_1), chr(self.byte_2)])
 
 
-def _decipher_parity_bit(byte: int):
-  """Extracts the byte value removing the odd parity bit"""
-  return byte & PARITY_BIT_MASK
+class SccLine:
+  """SCC line definition"""
 
+  def __init__(self, time_code: SccTimeCode, scc_words: List[SccWord]):
+    self.time_code = time_code
+    self.scc_words = scc_words
 
-def _decipher_hex_word(hex_word) -> (int, int, int):
-  """
-  Extracts hexadecimal word bytes and returns the value of each byte
-  and the value of the whole word as a tuple
-  """
-  if not _is_hex_word(hex_word):
-    raise ValueError("Expected a 2-bytes hexadecimal word, instead got ", hex_word)
+  @staticmethod
+  def from_str(line: str) -> Optional[SccLine]:
+    """Creates a SCC line instance from the specified string"""
+    if line == "":
+      return None
 
-  data = bytes.fromhex(hex_word)
-  byte_1 = _decipher_parity_bit(data[0])
-  byte_2 = _decipher_parity_bit(data[1])
-  word_value = byte_1 * 0x100 + byte_2
+    regex = re.compile(SCC_LINE_PATTERN)
+    match = regex.match(line)
 
-  return byte_1, byte_2, word_value
-
-
-def _word_to_chars(word_value: int) -> List[chr]:
-  """Converts a word value to a char array"""
-  if word_value > 0xFFFF:
-    raise ValueError("Expected a 2-bytes integer value, instead got ", word_value)
-
-  chars = word_value.to_bytes(2, byteorder='big')
-  return [chr(chars[0]), chr(chars[1])]
-
-
-def _read_word(hex_word: str) -> List[chr]:
-  """Reads the SCC hexadecimal word content"""
-  if hex_word == "":
-    return []
-
-  (byte_1, _byte_2, word_value) = _decipher_hex_word(hex_word)
-
-  if byte_1 < 0x20:
-    # XXXX: handle control codes
-    return []
-  else:
-    return _word_to_chars(word_value)
-
-
-def _read_line(context: _SccContext, line: str):
-  """Reads the SCC line content"""
-  if line == '':
-    return
-  chars = []
-
-  regex = re.compile(SCC_LINE_PATTERN)
-  match = regex.match(line)
-
-  if match:
-    paragraph = P()
-    context.count += 1
-    paragraph.set_id("caption" + str(context.count))
+    if not match:
+      return None
 
     time_code = match.group(1)
-    time_offset = time_codes.parse(time_code)
-    paragraph.set_begin(time_offset)
+    time_offset = SccTimeCode.parse(time_code)
 
     hex_words = line.split('\t')[1].split(' ')
-    for hex_word in hex_words:
-      chars += _read_word(hex_word)
+    scc_words = [SccWord.from_str(hex_word) for hex_word in hex_words if len(hex_word)]
+    return SccLine(time_offset, scc_words)
 
+  def to_model(self, context: _SccContext) -> P:
+    """Converts the SCC line to the data model"""
+
+    paragraph = P()
     paragraph.set_doc(context.div.get_doc())
-    context.div.push_child(paragraph)
+    context.count += 1
+    paragraph.set_id("caption" + str(context.count))
+    paragraph.set_begin(self.time_code.get_fraction())
+
+    text = ""
+    span = Span()
+    span.set_doc(context.div.get_doc())
+
+    for scc_word in self.scc_words:
+
+      if scc_word.byte_1 < 0x20:
+        # TODO: handle control codes
+        pass
+
+      else:
+        text += scc_word.to_text()
+
+    text_elem = Text(context.div.get_doc(), text)
+    span.push_child(text_elem)
+
+    paragraph.push_child(span)
+
+    return paragraph
 
 
 #
@@ -145,6 +181,12 @@ def to_model(scc_content: str):
   body.push_child(context.div)
 
   for line in scc_content.splitlines():
-    _read_line(context, line)
+    scc_line = SccLine.from_str(line)
+    if not scc_line:
+      continue
+
+    paragraph = scc_line.to_model(context)
+
+    context.div.push_child(paragraph)
 
   return document
