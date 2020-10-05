fd0247cabb30c5c51d5b6a43a82dbf947e784048
Valentin NOEL <valent.noel@gmail.com>
2020-10-05 16:22:13 +0200

    SCC: fixes drop-frame time code conversion to/from frames (#45)

--- src/main/python/ttconv/scc/time_codes.py
+++ src/main/python/ttconv/scc/time_codes.py
@@ -40,10 +40,10 @@ SMPTE_TIME_CODE_NDF_PATTERN = ':'.join(['(?P<ndf_h>[0-9]{2})',
                                         '(?P<ndf_s>[0-9]{2})',
                                         '(?P<ndf_f>[0-9]{2})'])
 
-SMPTE_TIME_CODE_DF_PATTERN = '(:|;|.)'.join(['(?P<df_h>[0-9]{2})',
-                                             '(?P<df_m>[0-9]{2})',
-                                             '(?P<df_s>[0-9]{2})',
-                                             '(?P<df_f>[0-9]{2})'])
+SMPTE_TIME_CODE_DF_PATTERN = '(:|;|.|,)'.join(['(?P<df_h>[0-9]{2})',
+                                               '(?P<df_m>[0-9]{2})',
+                                               '(?P<df_s>[0-9]{2})',
+                                               '(?P<df_f>[0-9]{2})'])
 
 
 class SccTimeCode:
@@ -79,7 +79,30 @@ class SccTimeCode:
 
   @staticmethod
   def _from_frames(nb_frames: int, frame_rate: typing.Optional[Fraction] = None, drop_frame=False) -> (int, int, int, int, bool):
-    frame_rate = frame_rate if frame_rate else DEFAULT_DF_FRAME_RATE if drop_frame else DEFAULT_NDF_FRAME_RATE
+
+    if drop_frame:
+      # add two dropped frames every minute, but not when the minute count is divisible by 10
+
+      selected_frame_rate = frame_rate if frame_rate else DEFAULT_DF_FRAME_RATE
+
+      nb_frames_in_one_minute = 60 * selected_frame_rate  # 1798 at 29.97 fps
+      nb_frames_in_ten_minutes = round(10 * nb_frames_in_one_minute)  # 17982 at 29.97 fps
+      drop_frames_per_minute = round(60 * (DEFAULT_NDF_FRAME_RATE - selected_frame_rate))  # 2 at 29.97 fps
+
+      nb_of_minute_tens = floor(nb_frames / nb_frames_in_ten_minutes)
+      nb_of_remaining_frames = round(nb_frames % nb_frames_in_ten_minutes)
+
+      nb_of_drop_frames_in_tens = drop_frames_per_minute * 9 * nb_of_minute_tens
+
+      nb_of_remaining_minutes = floor((nb_of_remaining_frames - drop_frames_per_minute) / round(nb_frames_in_one_minute))
+      if nb_of_remaining_minutes < 0:
+        nb_of_remaining_minutes = 0
+
+      nb_of_drop_frames_in_minutes = nb_of_remaining_minutes * drop_frames_per_minute
+
+      nb_frames += nb_of_drop_frames_in_tens + nb_of_drop_frames_in_minutes
+
+    frame_rate = frame_rate if frame_rate else DEFAULT_NDF_FRAME_RATE
 
     h = floor(nb_frames / (60 * 60 * frame_rate))
     m = floor(nb_frames / (60 * frame_rate)) % 60
@@ -93,6 +116,15 @@ class SccTimeCode:
     """Creates a SCC time code instance from the specified number of frames"""
     (h, m, s, f, drop_frame) = SccTimeCode._from_frames(nb_frames, frame_rate, drop_frame)
     return SccTimeCode(h, m, s, f, drop_frame)
+
+  @staticmethod
+  def from_fraction(fraction: Fraction, frame_rate: typing.Optional[Fraction] = None) -> SccTimeCode:
+    """Creates a SCC time code instance from the specified fraction"""
+    # Do not consider drop frame in the conversion
+    base_frame_rate = frame_rate if frame_rate else DEFAULT_NDF_FRAME_RATE
+
+    frames = fraction * base_frame_rate
+    return SccTimeCode.from_frames(frames, base_frame_rate)
 
   def get_hours(self) -> int:
     """Returns time code hours"""
@@ -110,10 +142,38 @@ class SccTimeCode:
     """Returns time code frames"""
     return self._frames
 
+  def get_nb_frames(self, frame_rate: typing.Optional[Fraction] = None) -> int:
+    """Returns the time code in number of frames"""
+    dropped_frames = 0
+    if self.is_drop_frame():
+      selected_frame_rate = frame_rate if frame_rate else DEFAULT_DF_FRAME_RATE
+
+      drop_frames_per_minute = round(60 * (DEFAULT_NDF_FRAME_RATE - selected_frame_rate))  # 2 at 29.97 fps
+
+      nb_of_minute_tens = self._hours * 6 + floor(self._minutes / 10)
+      nb_of_drop_frames_in_tens = drop_frames_per_minute * 9 * nb_of_minute_tens
+      remaining_minutes = self._minutes % 10
+      nb_of_drop_frames_in_remaining = drop_frames_per_minute * remaining_minutes
+
+      dropped_frames = nb_of_drop_frames_in_tens + nb_of_drop_frames_in_remaining
+
+    frame_rate = frame_rate if frame_rate else DEFAULT_NDF_FRAME_RATE
+    return (self._hours * 3600 + self._minutes * 60 + self._seconds) * frame_rate + self._frames - dropped_frames
+
+  def get_fraction(self, frame_rate: typing.Optional[Fraction] = None) -> Fraction:
+    """Converts the time code in a second-based fraction"""
+
+    # Do not consider drop frame in the conversion
+    base_frame_rate = frame_rate if frame_rate else DEFAULT_NDF_FRAME_RATE
+
+    return Fraction(self.get_nb_frames(frame_rate), base_frame_rate)
+
   def add_frames(self, nb_frames=1):
     """Add frames to the current time code"""
-    frames = self._get_frames() + nb_frames
+    frames = self.get_nb_frames() + nb_frames
+
     (h, m, s, f, _drop_frame) = SccTimeCode._from_frames(frames, drop_frame=self._drop_frame)
+
     self._hours = h
     self._minutes = m
     self._seconds = s
@@ -123,28 +183,15 @@ class SccTimeCode:
     """Returns whether the time code is drop-frame or not"""
     return self._drop_frame
 
-  def _get_frames(self, frame_rate: typing.Optional[Fraction] = None) -> int:
-    frame_rate = self._get_frame_rate(frame_rate)
-    return (self._hours * 3600 + self._minutes * 60 + self._seconds) * frame_rate + self._frames
-
-  def _get_frame_rate(self, frame_rate: typing.Optional[Fraction] = None) -> Fraction:
-    if frame_rate:
-      return frame_rate
-
-    if self.is_drop_frame():
-      return DEFAULT_DF_FRAME_RATE
-
-    return DEFAULT_NDF_FRAME_RATE
-
-  def get_fraction(self, frame_rate: typing.Optional[Fraction] = None) -> Fraction:
-    """Converts the time code in a second-based fraction"""
-
-    # Do not consider drop frame in the conversion
-    base_frame_rate = frame_rate if frame_rate else DEFAULT_NDF_FRAME_RATE
-
-    return Fraction(self._get_frames(frame_rate), base_frame_rate)
-
   def __repr__(self):
     if self.is_drop_frame():
       return ":".join(f'{item:02}' for item in [self._hours, self._minutes, self._seconds]) + ";" + f'{self._frames:02}'
     return ":".join(f'{item:02}' for item in [self._hours, self._minutes, self._seconds, self._frames])
+
+  def __eq__(self, other: SccTimeCode):
+    if not isinstance(other, SccTimeCode):
+      return False
+    return self._hours == other.get_hours() and \
+           self._minutes == other.get_minutes() and \
+           self._seconds == other.get_seconds() and \
+           self._frames == other.get_frames()
