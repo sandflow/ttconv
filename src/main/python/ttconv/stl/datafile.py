4501c4506edb1055274bb50c049cb693661807df
Pierre-Anthony Lemieux <pal@palemieux.com>
2021-08-18 11:17:03 -0700

    Fix region height with double height characters

--- src/main/python/ttconv/stl/datafile.py
+++ src/main/python/ttconv/stl/datafile.py
@@ -190,6 +190,8 @@ class DataFile:
 
     self.tti_tf += tti.get_tf().strip(b'\x8f')
 
+    is_double_height_characters = ttconv.stl.tf.is_double_height(self.tti_tf)
+
     # continue accumulating if we have an extension block
 
     if tti.get_ebn() != 0xFF:
@@ -283,8 +285,12 @@ class DataFile:
 
         else:
 
+          line_count = ttconv.stl.tf.line_count(self.tti_tf)
+          vp = tti.get_vp()
+          line_height = 2 if is_double_height_characters else 1
+
           r_y = DEFAULT_VERTICAL_SAFE_MARGIN_PCT
-          r_height = ((tti.get_vp() + ttconv.stl.tf.line_count(self.tti_tf) - 1)/ DEFAULT_TELETEXT_ROWS) * safe_area_height
+          r_height = ((vp + line_count * line_height - 1)/ DEFAULT_TELETEXT_ROWS) * safe_area_height
           
           region = get_region(
             self.doc,
