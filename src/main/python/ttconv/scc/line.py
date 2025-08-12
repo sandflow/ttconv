600461ef3388185b96db6a05ec2eaf4687f6347a
Pierre-Anthony Lemieux <pal@palemieux.com>
2025-08-12 08:49:36 -0700

    Further improvements

--- src/main/python/ttconv/scc/line.py
+++ src/main/python/ttconv/scc/line.py
@@ -109,13 +109,8 @@ class SccLine:
 
     debug = str(self.time_code) + "\t"
 
-    first = True
-
     for scc_word in self.scc_words:
-
-      if not first:
-        self.time_code.add_frames()
-      first = False
+      self.time_code.add_frames()
 
       if context.previous_word is not None and context.previous_word.value == scc_word.value and context.previous_word.is_code():
         context.previous_word = None
